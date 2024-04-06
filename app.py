6596e5e75131e874c967b4ea938c69eea5aaf61c
kevinleanderlouis <158525618+kevinleanderlouis@users.noreply.github.com>
2024-04-06 01:17:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud():
-  return 'Hello from Kevin ECS Container.'
+  return 'Hello from Kevin Leander ECS Container.'
 
 app.run(host='0.0.0.0')
